620305780d09cea723ceb6e1695a04096038c0a2
Alexandre Abdu El-moor <alexandre.elmoor@gmail.com>
2023-01-10 19:31:17 -0300

    06 - CRUD Alunos

---
+++ src/controllers/AlunoController.js
@@ -0,0 +1,91 @@
+import Aluno from '../models/Aluno';
+
+class AlunoController {
+  async index(req, res) {
+    const alunos = await Aluno.findAll();
+    res.json(alunos);
+  }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno inexistente'],
+        });
+      }
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno inexistente'],
+        });
+      }
+      await aluno.destroy();
+      return res.json({
+        apagado: true,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno inexistente'],
+        });
+      }
+      const alunoAlterado = await aluno.update(req.body);
+      return res.json(alunoAlterado);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+}
+
+export default new AlunoController();
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Vanilza',
-      sobrenome: 'Dario El-moor',
-      email: 'vanilza.elmoor@gmail.com',
-      idade: 70,
-      peso: 83,
-      altura: 1.55,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
